069ed5a3c185038a51f028f0f828bca4a6c7082c
Charles <rudahushac@gmail.com>
2021-08-12 20:53:46 +0200

    Stream API Assignment.

---
+++ stream_assignment/src/main/java/se/lexicon/vxo/App.java
@@ -0,0 +1,16 @@
+package se.lexicon.vxo;
+
+
+import se.lexicon.vxo.service.PeopleImpl;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class App
+{
+    public static void main( String[] args ) {
+
+    }
+}
